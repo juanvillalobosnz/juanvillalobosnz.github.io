078ce195b67cc553c7243f3e979682437b2b2d6b
jairvillalobos <53276949+jairvillalobos@users.noreply.github.com>
2024-01-05 11:57:25 -0500

    Update translations.js

--- js/translations.js
+++ js/translations.js
@@ -3,7 +3,7 @@ const translations = {
     en: {
       home: "Home",
       projects: "Projects",
-      tagline: "Backend Engineer (APIs & Microservices) | Go, Python & TypeScript | AWS, Docker, Kubernetes | Security Enthusiast",
+      tagline: "Full Stack Engineer (APIs & Microservices) | Go, Python & TypeScript | AWS, Docker, Kubernetes | Security Enthusiast",
       btncv:"Download CV",
       titleexperience: "Work experience",
       pworkExperience: "More than 5 years of experience in back-end development. Specialist in REST APIs, databases and languages such as React, Golang and Python. Knowledge in IT technical support.Design and implementation of scalable RESTful APIs with focus on performance, security and best practices. Integration with API Gateways through plugin development. Integral participation in the lifecycle of high traffic web and mobile applications. Agile methodologies. Commitment to continuous learning and professional growth. Focus on delivering high value technology solutions that enhance business success.", educationtitle: "Education",
